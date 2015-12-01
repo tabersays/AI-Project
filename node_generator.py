d339fcd02e7e711158b84a6b48e9edad57ecfac9
Michael Taylor <90michaeltaylor@gmail.com>
2015-11-30 20:40:58 -0800

    Created a node gernerating script and started out training function in our main.

---
+++ node_generator.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+#author Michael Taylor
+#date November 30th 2015
+
+puts "Name of node file?"
+file_name = gets.gsub(/\s+/, "")
+puts "How many input nodes?"
+in_nodes = gets.to_i
+puts "How many hidden nodes?"
+hidden_nodes = gets.to_i
+puts "Reduction step?"
+reduction_step = gets.to_i
+
+node = File.new(file_name, "w")
+node.write("#{in_nodes}\n#{hidden_nodes}\n")
+
+#first hidden layer conected to the input layer
+for i in 0..hidden_nodes - 1
+    node_data = "1.0"
+    for j in 0..in_nodes - 1
+        node_data = node_data + " " + rand(0.0...1.0).to_s
+    end
+    node_data = node_data + "@\n"
+    node.write(node_data)
+end
+node.write("%\n")
+
+#all other hidden layers
+count = hidden_nodes - reduction_step
+last_node_layer = 0
+puts "Node count =  #{count}."
+while count > 0
+    last_node_layer = count
+    for i in 0..count - 1
+        node_data = "1.0"
+        for j in 0..count
+             node_data = node_data + " " + rand(0.0...1.0).to_s
+        end
+        node_data = node_data + "@\n"
+        node.write(node_data)
+    end
+    node.write("%\n")
+    count -= reduction_step
+    puts "Node count =  #{count}."
+end
+
+if last_node_layer != 1
+    node_data = "1.0"
+    for i in 0..last_node_layer - 1
+        node_data = node_data + " " + rand(0.0...1.0).to_s
+    end
+    node_data = node_data + "@\n"
+    node.write(node_data)
+    node.write("%\n")
+end
