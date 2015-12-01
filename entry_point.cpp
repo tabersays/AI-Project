01dc0ea84a104eb1b329534c0371e924b095fcba
Thomas Russel Carrel <goshisanniichi@yahoo.com or tcarrel@mail.csuchico.edu>
2015-11-30 23:38:09 -0800

    Changes some return types and removed some errors from main().

--- entry_point.cpp
+++ entry_point.cpp
@@ -20,71 +20,74 @@ using namespace std;
 #include"ANN.h"
 #include"directory.h"
 
-bool run_net( )// ANN& network )
+int run_net( ANN& /*network*/ )
 {
 
-  return false;
+    return 0;
 }
 
-bool train_net( ANN& network, string name )
+int train_net( ANN& /*network*/, string name )
 {
-  Directory dir(name);
-  cout << "File index to start at, should be 0 unless crash?" << endl;
-  unsigned file_index = 0;
-  cin >> file_index;
-  cout << "Number of training steps, 0 to go through the entire directory."
-    << endl;
-  unsigned step = 0;
-  cin >> step;
+    Directory dir(name);
+    cout << "File index to start at, should be 0 unless crash?" << endl;
+    unsigned file_index = 0;
+    cin >> file_index;
+    cout << "Number of training steps, 0 to go through the entire directory."
+        << endl;
+    unsigned step = 0;
+    cin >> step;
 
-  // Set step to size of oyr training set.
-  if(step == 0)
-    step = dir.file().size();
+    // Set step to size of oyr training set.
+    if(step == 0)
+        step = dir.file().size();
 
 
-  ofstream training_progress;
-  training_progress.open( "ann_training_step" );
+    ofstream training_progress;
+    training_progress.open( "ann_training_step" );
 
-  if( !training_progress.is_open() )
-    return false;
+    if( !training_progress.is_open() )
+        return 1;
 
-  for(unsigned i = 0; i < step; --i)
-  {
-
-    while( dir.file().size() )// Number of training iterations
+    for(unsigned i = 0; i < step; --i)
     {
 
+        while( dir.file().size() )// Number of training iterations
+        {
 
+
+        }
     }
-  }
-  return false;
+    return 0;
 }
 
 int main( int argc, char* argv[] )
 {
-  if( argc < 2 )
-  {
-    cerr
-      << "\tUsage:" << endl
-      << "\t\t" << argv[0] << " <filename> [optional]<directory>" << endl
-      << "\t\t\t<filename>\tThe saved ANN configuration file." << endl
-      << "\t\t\t<directory>\tThe directory containing training data.\n"
-      << endl;
-    return 1;
-  }
+    if( argc < 2 )
+    {
+        cerr
+            << "\tUsage:" << endl
+            << "\t\t" << argv[0] << " <filename> [optional]<directory>" << endl
+            << "\t\t\t<filename>\tThe saved ANN configuration file." << endl
+            << "\t\t\t<directory>\tThe directory containing training data.\n"
+            << endl;
+        return 1;
+    }
 
-  ANN network( argv[1] );
+    ANN network( argv[1] );
 
-  switch( argc )
-  {
-    case 2:
-      string dir = argv[2];
-      return run_net( network, dir );
-    case 3:
-      //            return train_net( network );
-      cerr << 3; return 0;
-    default:
-      cerr << "\tToo many arguments.\n" << endl;
-      return 1;
-  }
+    // Cannot create variables within a switch statement.
+    string dir;
+    if( argc == 3 )
+        dir = argv[2];
+
+    switch( argc )
+    {
+        case 2:
+            return run_net( network );
+        case 3:
+            return train_net( network, dir );            
+        default:
+            cerr << "\tToo many arguments.\n" << endl;
+            return 1;
+    }
 }
